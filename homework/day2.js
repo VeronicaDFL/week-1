ae6356d955280be142f7f2e261bc2ee79da9767b
Veronica De Felice <verodefelic@gmail.com>
2023-03-22 08:50:29 -0400

    pushing homework

--- homework/day2.js
+++ homework/day2.js
@@ -1,21 +1,41 @@
-const pet = 'sheep';
+// const pet = 'sheep';
 
-switch (pet) {
-    case 'cat':
-        console.log('meow');
-    break;
+// switch (pet) {
+// //     case 'cat':
+//         console.log('meow');
+//     break;
 
-    case 'lion':
-        console.log('roarrr');
-    break;
+//     case 'lion':
+//         console.log('roarrr');
+//     break;
 
-case 'sheep':
-case 'goat':
-console.log('beeee');
-break 
+// case 'sheep':
+// case 'goat':
+// console.log('beeee');
+// break 
 
-default:
-console.log('animals')
+// default:
+// console.log('animals')
+// }
+
+
+// ////////////////////////////////////////  homework #2   ////////////////////////////////////////////////////////////////
+
+// function message (fruit) {
+
+//     console.log("Red " + fruit);
+
+// }
+// message ("Strawberry");
+
+//////////////////////////////////////    homework #3   ///////////////////////////////////////////////////
+
+
+function totSum (number1, number2){
+
+    return number1 + number2;
 }
+
+console.log(totSum ( 57, 43 );
 
 
