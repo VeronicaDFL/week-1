64439293d557ad139c7792ffc91d1e436032eab3
Veronica De Felice <verodefelic@gmail.com>
2023-03-21 23:27:56 -0400

    Day 2 Homework one

---
+++ homework/day2.js
@@ -0,0 +1,21 @@
+const pet = 'sheep';
+
+switch (pet) {
+    case 'cat':
+        console.log('meow');
+    break;
+
+    case 'lion':
+        console.log('roarrr');
+    break;
+
+case 'sheep':
+case 'goat':
+console.log('beeee');
+break 
+
+default:
+console.log('animals')
+}
+
+
